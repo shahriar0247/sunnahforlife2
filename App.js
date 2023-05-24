d20a14fb7a404e2941523390d4ea8701d994c6e7
Ahmed <ashahriar@telware.com>
2023-05-24 12:47:51 +0600

    listing collections

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [all_collections, set_all_collections] = useState([]);
+
+	async function fetch_collections() {
+		const response = await fetch("http://localhost:5000/v1/collections");
+		const response_json = await response.json();
+		set_all_collections(response_json);
+	}
+
+	useEffect(() => {
+		fetch_collections();
+	}, []);
+
+  try {
+    var all_collection_elements = all_collections["data"].map(function (one_collection) {
+      return (
+        <View>
+          <Text>{one_collection["name"]}</Text>
+        </View>
+      );
+    });
+  
+  } catch (error) {
+    var all_collection_elements = <View></View>
+  }
+
+  
+	return (
+		<View style={styles.container}>
+			{all_collection_elements}
+			<Text>sadsadad</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
